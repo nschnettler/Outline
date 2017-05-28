90801da387a5aaecd74fd4ee61686c65636a97ed
Sh4dowSoul <niklas.schnettler@gmail.com>
2017-05-28 14:08:40 +0200

    Revert "Merge: Release 9.0.0: Full Kotlin Integration"
    This reverts commit 9c2ca207e9498c50037308a8c17b0462e3fb1e32.

--- app/src/main/java/substratum/theme/template/internal/TBOConstants.java
+++ app/src/main/java/substratum/theme/template/internal/TBOConstants.java
@@ -1,8 +1,12 @@
-package substratum.theme.template.internal
+package substratum.theme.template.internal;
 
-object TBOConstants {
+/**
+ * Created by Nicholas on 5/5/2017.
+ */
 
-    val THEME_READY_PACKAGES = arrayOf(
+public class TBOConstants {
+
+    public static final String[] THEME_READY_PACKAGES = new String[]{
             "com.google.android.gm",
             "com.google.android.googlequicksearchbox",
             "com.android.vending",
@@ -11,10 +15,10 @@ object TBOConstants {
             "com.google.android.youtube",
             "com.google.android.apps.photos",
             "com.google.android.inputmethod.latin"
-    )
+    };
 
-    val EXTRA_PACKAGE_NAMES = arrayOf(
+    public static final String[] EXTRA_PACKAGE_NAMES = new String[]{
             "com.google.android.contacts",
             "com.google.android.dialer"
-    )
+    };
 }
