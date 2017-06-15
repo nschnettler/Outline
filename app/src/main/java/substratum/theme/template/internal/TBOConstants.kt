3e5c127a8dab350f333f83a460646d1c50a68076
Sh4dowSoul <niklas.schnettler@gmail.com>
2017-06-15 14:41:30 +0200

    Project: Use Template 10 as Base

---
+++ app/src/main/java/substratum/theme/template/internal/TBOConstants.kt
@@ -0,0 +1,20 @@
+package substratum.theme.template.internal
+
+object TBOConstants {
+
+    val THEME_READY_PACKAGES = arrayOf(
+            "com.google.android.gm",
+            "com.google.android.googlequicksearchbox",
+            "com.android.vending",
+            "com.google.android.apps.plus",
+            "com.google.android.talk",
+            "com.google.android.youtube",
+            "com.google.android.apps.photos",
+            "com.google.android.inputmethod.latin"
+    )
+
+    val EXTRA_PACKAGE_NAMES = arrayOf(
+            "com.google.android.contacts",
+            "com.google.android.dialer"
+    )
+}
