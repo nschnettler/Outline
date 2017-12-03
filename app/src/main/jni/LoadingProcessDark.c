c3712c3207da58a61ead3da32cce5027cfe1ab25
Sh4dowSoul <niklas.schnettler@gmail.com>
2017-12-03 16:51:09 +0100

    Revert "Template: Update (11.5.0)"
    This reverts commit a69b576de53d84cf0914510a1bb0a147cd17d577.

--- app/src/main/jni/LoadingProcessDark.c
+++ app/src/main/jni/LoadingProcessDark.c
@@ -1,72 +1,87 @@
 #include <jni.h>
-#include <LoadingProcess.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
-
-// Make sure to fill in those values if you enable anti piracy
-char APK_SIGNATURE_PRODUCTION[] = "tGQYWSO6wsgTs90QJwLNH6vSwbA=";
-char BASE_64_LICENSE_KEY[] = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArTRaZnE2hkleiuajs6R41Fpf8f+em2T4cogtzPQ6O7r4DqBnUtc48KiZE408o0gGQoB3hr9vwoNKblxxSWxowcF9crDagEcWE6sfzHdEx/tM3nrVvq8jJPYPbUOfRPsxrPV0DqLGMU2DlxCeOhMWCRGK0IEMjQSBXXo33fie0g8sQrx7PGiaR9E0rbjO2eK5l7TsU7o1AhpONl4ODMcxLEupUEk3P8NxohafPSdLv/hFAJHDxzkJZCpvaxETcCAz8VatODZIKylHiYQsd0aEeYvfOmfpvG7PMz4o7H5riv1XE30qFFH0NVKXlYtgbewmhbmw5sRIcxuHmVQXTkIzWwIDAQAB";
-
-// You can change those values if you enable anti piracy
-jboolean ENABLE_APP_BLACKLIST_CHECK = JNI_FALSE;
-jboolean ENABLE_INTERNET_CHECK = JNI_FALSE;
-jboolean REQUIRE_INSTALL_FROM_PLAY_STORE = JNI_TRUE;
-jboolean REQUIRE_INSTALL_FROM_AMAZON_STORE = JNI_FALSE;
-
-// Change this value to JNI_FALSE to block users from using your theme with third party substratum build
-jboolean ALLOW_THIRD_PARTY_SUBSTRATUM_BUILD = JNI_TRUE;
-
 /*
- * APK Signature Production
+ * Piracy Check
+ *
+ * Change this value to JNI_TRUE if you would like to enable anti piracy
  */
-JNIEXPORT jstring JNICALL
-Java_com_schnettler_ethereal_SubstratumLauncher_getAPKSignatureProduction(JNIEnv *env) {
-    return (*env)->NewStringUTF(env, APK_SIGNATURE_PRODUCTION);
+JNIEXPORT jboolean JNICALL
+Java_com_schnettler_ethereal_SubstratumLauncher_getAPStatus(JNIEnv *env) {
+    return JNI_TRUE;
 }
 
 /*
  * Base 64 License Key
+ *
+ * Insert your base 64 license key obtained from Play Store in the quotations below!
+ * ATTENTION: On line 23, do you see the "000"? You MUST count the amount of characters of your key
+ * and input the proper number! This is so that you get your own specialized set of variables!
  */
 JNIEXPORT jstring JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_getBase64Key(JNIEnv *env) {
-    return (*env)->NewStringUTF(env, BASE_64_LICENSE_KEY);
+    // TODO: This must be done!
+    return (*env)->NewStringUTF(env, "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArTRaZnE2hkleiuajs6R41Fpf8f+em2T4cogtzPQ6O7r4DqBnUtc48KiZE408o0gGQoB3hr9vwoNKblxxSWxowcF9crDagEcWE6sfzHdEx/tM3nrVvq8jJPYPbUOfRPsxrPV0DqLGMU2DlxCeOhMWCRGK0IEMjQSBXXo33fie0g8sQrx7PGiaR9E0rbjO2eK5l7TsU7o1AhpONl4ODMcxLEupUEk3P8NxohafPSdLv/hFAJHDxzkJZCpvaxETcCAz8VatODZIKylHiYQsd0aEeYvfOmfpvG7PMz4o7H5riv1XE30qFFH0NVKXlYtgbewmhbmw5sRIcxuHmVQXTkIzWwIDAQAB");
+}
+
+/*
+ * APK Signature Production
+ *
+ * Insert your release APK's signature in the quotations below!
+ * ATTENTION: On line 34, do you see the "28"? If you're disabling antipiracy, change that to 0!
+ * All release production signature prefixes have the length of 28!
+ */
+JNIEXPORT jstring JNICALL
+Java_com_schnettler_ethereal_SubstratumLauncher_getAPKSignatureProduction(JNIEnv *env) {
+    return (*env)->NewStringUTF(env, "tGQYWSO6wsgTs90QJwLNH6vSwbA=");
 }
 
 /*
  * Enforce Internet Check
+ *
+ * Change this value to JNI_TRUE if you would like to enable internet check
  */
 JNIEXPORT jboolean JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_getInternetCheck(JNIEnv *env) {
-    return ENABLE_INTERNET_CHECK;
+    return JNI_FALSE;
 }
 
 /*
  * Enforce Google Play Install
+ *
+ * Change this value to JNI_TRUE if you would like to enable this check
  */
 JNIEXPORT jboolean JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_getGooglePlayRequirement(JNIEnv *env) {
-    return REQUIRE_INSTALL_FROM_PLAY_STORE;
+    return JNI_TRUE;
 }
 
 /*
  * Enforce Amazon App Store Install
+ *
+ * Change this value to JNI_TRUE if you would like to enable this check
  */
 JNIEXPORT jboolean JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_getAmazonAppStoreRequirement(JNIEnv *env) {
-    return REQUIRE_INSTALL_FROM_AMAZON_STORE;
+    return JNI_FALSE;
 }
 
 /*
  * Enable check for Blacklisted APKs
+ *
+ * Change this value to JNI_TRUE if you would like to enable blacklist check
  */
 JNIEXPORT jboolean JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_getBlacklistedApplications(JNIEnv *env) {
-    return ENABLE_APP_BLACKLIST_CHECK;
+    return JNI_FALSE;
 }
 
 /*
  * Allow Third Party Substratum Builds
+ *
+ * Change this value to JNI_FALSE if you would like to ban your theme to work on external, non-team
+ * built compilations of substratum
  *
  * WARNING: Having this enabled may or may not cause a bunch of issues due to the system not built
  *          and distributed by an official team member. You will take charge of handling bug reports
@@ -74,21 +89,17 @@ Java_com_schnettler_ethereal_SubstratumLauncher_getBlacklistedApplications(JNIEn
  */
 JNIEXPORT jboolean JNICALL
 Java_com_schnettler_ethereal_SubstratumLauncher_allowThirdPartySubstratumBuilds(JNIEnv *env) {
-    return ALLOW_THIRD_PARTY_SUBSTRATUM_BUILD;
+    return JNI_FALSE;
 }
 
-JNIEXPORT jbyteArray JNICALL
-Java_com_schnettler_ethereal_SubstratumLauncher_getDecryptionKey(JNIEnv *env) {
-    jbyteArray ret = (*env)->NewByteArray(env, 16);
-    (*env)->SetByteArrayRegion(env, ret, 0, 16, DECRYPTION_KEY);
-    return ret;
-}
-
-JNIEXPORT jbyteArray JNICALL
-Java_com_schnettler_ethereal_SubstratumLauncher_getIVKey(JNIEnv *env) {
-    jbyteArray ret = (*env)->NewByteArray(env, 16);
-    (*env)->SetByteArrayRegion(env, ret, 0, 16, IV_KEY);
-    return ret;
+/*
+ * Enable Samsung theming
+ *
+ * Change Line 94 to JNI_FALSE for official Samsung support!
+ */
+JNIEXPORT jboolean JNICALL
+Java_substratum_theme_template_SubstratumLauncher_getSamsungSupport(JNIEnv *env) {
+    return Java_com_schnettler_ethereal_SubstratumLauncher_allowThirdPartySubstratumBuilds(env);
 }
 
 #pragma clang diagnostic pop
