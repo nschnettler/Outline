5071cff67eb4c0a57fa8f155718b065bab698c9c
Niklas Schnettler <niklas.schnettler@gmail.com>
2020-11-21 13:57:22 +0100

    Remove AntiPiracy Check
    (cherry picked from commit f8f3d9da060ffe2970ecd0e844f1546009ee0385)

--- app/src/main/java/com/schnettler/common/AdvancedConstants.kt
+++ app/src/main/java/com/schnettler/common/AdvancedConstants.kt
@@ -2,50 +2,6 @@ package com.schnettler.common
 
 object AdvancedConstants {
 
-    // Dynamic filter that only works on Substratum 627+
-    // WARNING: Only enable if you are sure you want certification status to pass on Substratum
-    //          before launching the theme, or else it will throw an unauthorized toast!
-    const internal val SUBSTRATUM_FILTER_CHECK = false
-
-    // Miscellaneous Checks
-    const val ENFORCE_MINIMUM_SUBSTRATUM_VERSION = true
-    const val MINIMUM_SUBSTRATUM_VERSION = 712 // 510 is the final MM build
-
-    // Custom message on theme launch, see theme_strings.xml for changing the dialog content
-    // Set SHOW_DIALOG_REPEATEDLY to true if you want the dialog to be showed on every theme launch
-    const val SHOW_LAUNCH_DIALOG = false
-    const val SHOW_DIALOG_REPEATEDLY = false
-
-    // Blacklisted APKs to prevent theme launching, these include simple regex formatting, without
-    // full regex formatting (e.g. com.android. will block everything that starts with com.android.)
-    val BLACKLISTED_APPLICATIONS = arrayOf(
-            "cc.madkite.freedom",
-            "zone.jasi2169.uretpatcher",
-            "uret.jasi2169.patcher",
-            "p.jasi2169.al3",
-            "com.dimonvideo.luckypatcher",
-            "com.chelpus.lackypatch",
-            "com.forpda.lp",
-            "com.android.vending.billing.InAppBillingService",
-            "com.android.vending.billing.InAppBillingSorvice",
-            "com.android.vendinc",
-            "com.appcake",
-            "ac.market.store",
-            "org.sbtools.gamehack",
-            "com.zune.gamekiller",
-            "com.aag.killer",
-            "com.killerapp.gamekiller",
-            "cn.lm.sq",
-            "net.schwarzis.game_cih",
-            "org.creeplays.hack",
-            "com.baseappfull.fwd",
-            "com.zmapp",
-            "com.dv.marketmod.installer",
-            "org.mobilism.android",
-            "com.blackmartalpha",
-            "org.blackmart.market"
-    )
-
     // List of all organization theming systems officially supported by the team
     val ORGANIZATION_THEME_SYSTEMS = arrayOf(
             "projekt.substratum",
--- app/src/main/java/com/schnettler/common/CommonLauncher.kt
+++ app/src/main/java/com/schnettler/common/CommonLauncher.kt
@@ -7,18 +7,13 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.github.javiersantos.piracychecker.*
-import com.github.javiersantos.piracychecker.enums.InstallerID
-import com.github.javiersantos.piracychecker.utils.apkSignature
 import com.schnettler.common.AdvancedConstants.ORGANIZATION_THEME_SYSTEMS
 import com.schnettler.common.AdvancedConstants.OTHER_THEME_SYSTEMS
-import com.schnettler.common.ThemeFunctions.checkApprovedSignature
 import com.schnettler.common.ThemeFunctions.getSelfSignature
 import com.schnettler.common.ThemeFunctions.isCallingPackageAllowed
 
 object CommonLauncher {
 
-    private const val debug = false
     private const val tag = "SubstratumThemeReport"
     private const val substratumIntentData = "projekt.substratum.THEME"
     private const val getKeysIntent = "projekt.substratum.GET_KEYS"
@@ -26,31 +21,15 @@ object CommonLauncher {
     private const val themePiracyCheck = false
 
     fun start(ctx : Activity) {
-        /* STEP 1: Block hijackers */
         val caller = ctx.callingActivity!!.packageName
-        val organizationsSystem = ORGANIZATION_THEME_SYSTEMS.contains(caller)
-        val supportedSystem = organizationsSystem || OTHER_THEME_SYSTEMS.contains(caller)
-        if (!BuildConfig.SUPPORTS_THIRD_PARTY_SYSTEMS && !supportedSystem) {
-            Log.e(tag, "This theme does not support the launching theme system. [HIJACK] ($caller)")
-            Toast.makeText(ctx,
-                    String.format(ctx.getString(R.string.unauthorized_theme_client_hijack), caller),
-                    Toast.LENGTH_LONG).show()
-            ctx.finish()
-        }
-        if (debug) {
-            Log.d(tag, "'$caller' has been authorized to launch this theme. (Phase 1)")
-        }
 
         /* STEP 2: Ensure that our support is added where it belongs */
         val action = ctx.intent.action
         var verified = false
         if ((action == substratumIntentData) or (action == getKeysIntent)) {
             // Assume this called from organization's app
-            if (organizationsSystem) {
-                verified = when {
-                    BuildConfig.ALLOW_THIRD_PARTY_SUBSTRATUM_BUILDS -> true
-                    else -> checkApprovedSignature(ctx, caller)
-                }
+            if (ORGANIZATION_THEME_SYSTEMS.contains(caller)) {
+                verified = true
             }
         } else {
             OTHER_THEME_SYSTEMS
@@ -63,9 +42,6 @@ object CommonLauncher {
             ctx.finish()
             return
         }
-        if (debug) {
-            Log.d(tag, "'$action' has been authorized to launch this theme. (Phase 2)")
-        }
 
         /* STEP 3: Do da thang */
         val sharedPref = ctx.getSharedPreferences(null, 0)
@@ -73,81 +49,43 @@ object CommonLauncher {
             showDialog(ctx)
             sharedPref.edit().putBoolean("dialog_showed", true).apply()
         } else {
-            startAntiPiracyCheck(ctx)
+            openTheme(ctx)
         }
     }
 
-    private fun startAntiPiracyCheck(ctx : Activity) {
-        if (BuildConfig.BASE_64_LICENSE_KEY.isEmpty() && debug && !BuildConfig.DEBUG) {
-            Log.e(tag, ctx.apkSignature)
-        }
-
-        if (!themePiracyCheck) {
-            ctx.piracyChecker {
-                if (BuildConfig.ENFORCE_GOOGLE_PLAY_INSTALL) {
-                    enableInstallerId(InstallerID.GOOGLE_PLAY)
-                }
-                if (BuildConfig.BASE_64_LICENSE_KEY.isNotEmpty()) {
-                    enableGooglePlayLicensing(BuildConfig.BASE_64_LICENSE_KEY)
-                }
-                if (BuildConfig.APK_SIGNATURE_PRODUCTION.isNotEmpty()) {
-                    enableSigningCertificate(BuildConfig.APK_SIGNATURE_PRODUCTION)
-                }
-                callback {
-                    allow {
-                        val returnIntent = if (ctx.intent.action == getKeysIntent) {
-                            Intent(receiveKeysIntent)
-                        } else {
-                            Intent()
-                        }
-
-                        val themeName = ctx.getString(R.string.ThemeName)
-                        val themeAuthor = ctx.getString(R.string.ThemeAuthor)
-                        val themePid = ctx.packageName
-                        returnIntent.putExtra("theme_name", themeName)
-                        returnIntent.putExtra("theme_author", themeAuthor)
-                        returnIntent.putExtra("theme_pid", themePid)
-                        returnIntent.putExtra("theme_debug", BuildConfig.DEBUG)
-                        returnIntent.putExtra("theme_piracy_check", themePiracyCheck)
-                        returnIntent.putExtra("encryption_key", "")
-                        returnIntent.putExtra("iv_encrypt_key", "")
-
-                        val callingPackage = ctx.intent.getStringExtra("calling_package_name")
-                        if (!isCallingPackageAllowed(callingPackage)) {
-                            ctx.finish()
-                        } else {
-                            returnIntent.`package` = callingPackage
-                        }
-
-
-                        if (ctx.intent.action == substratumIntentData) {
-                            ctx.setResult(getSelfSignature(ctx.applicationContext), returnIntent)
-                        } else if (ctx.intent.action == getKeysIntent) {
-                            returnIntent.action = receiveKeysIntent
-                            ctx.sendBroadcast(returnIntent)
-                        }
-                        destroy()
-                        ctx.finish()
-                    }
-                    doNotAllow { _, _ ->
-                        val parse = String.format(
-                                ctx.getString(R.string.toast_unlicensed),
-                                ctx.getString(R.string.ThemeName))
-                        Toast.makeText(ctx, parse, Toast.LENGTH_SHORT).show()
-                        destroy()
-                        ctx.finish()
-                    }
-                    onError { error ->
-                        Toast.makeText(ctx, error.toString(), Toast.LENGTH_LONG)
-                                .show()
-                        ctx.finish()
-                    }
-                }
-            }.start()
+    private fun openTheme(ctx : Activity) {
+        val returnIntent = if (ctx.intent.action == getKeysIntent) {
+            Intent(receiveKeysIntent)
         } else {
-            Toast.makeText(ctx, R.string.unauthorized, Toast.LENGTH_LONG).show()
-            ctx.finish()
+            Intent()
         }
+
+        val themeName = ctx.getString(R.string.ThemeName)
+        val themeAuthor = ctx.getString(R.string.ThemeAuthor)
+        val themePid = ctx.packageName
+        returnIntent.putExtra("theme_name", themeName)
+        returnIntent.putExtra("theme_author", themeAuthor)
+        returnIntent.putExtra("theme_pid", themePid)
+        returnIntent.putExtra("theme_debug", BuildConfig.DEBUG)
+        returnIntent.putExtra("theme_piracy_check", themePiracyCheck)
+        returnIntent.putExtra("encryption_key", "")
+        returnIntent.putExtra("iv_encrypt_key", "")
+
+        val callingPackage = ctx.intent.getStringExtra("calling_package_name")
+        if (!isCallingPackageAllowed(callingPackage)) {
+            ctx.finish()
+        } else {
+            returnIntent.`package` = callingPackage
+        }
+
+
+        if (ctx.intent.action == substratumIntentData) {
+            ctx.setResult(getSelfSignature(ctx.applicationContext), returnIntent)
+        } else if (ctx.intent.action == getKeysIntent) {
+            returnIntent.action = receiveKeysIntent
+            ctx.sendBroadcast(returnIntent)
+        }
+        ctx.finish()
     }
 
     private fun showDialog(ctx: Activity) {
@@ -155,7 +93,7 @@ object CommonLauncher {
                 .setCancelable(false)
                 .setTitle(R.string.launch_dialog_title)
                 .setMessage(R.string.launch_dialog_content)
-                .setPositiveButton(android.R.string.ok) { _, _ -> startAntiPiracyCheck(ctx) }
+                .setPositiveButton(android.R.string.ok) { _, _ -> openTheme(ctx) }
         dialog.show()
     }
 }
